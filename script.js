aa189c5254e02d9c10905f4ce26fe3db85fc5249
Mofachhal Hossain <mofachhalhossain101@gmail.com>
2021-08-22 19:36:03 +0600

    question answer data added v1.0

---
+++ script.js
@@ -0,0 +1,83 @@
+
+const gameDB = [
+    {
+        question: "What is the name of this bird",
+        a: "Woodpecker",
+        b: "Guinea Fowl",
+        c: "Spoonbill",
+        d: "Columbidae",
+        ans: "ans1",
+    },
+
+    {
+        question: "What is the name of this fish",
+        a: "Wels Catfish",
+        b: "Common Carp",
+        c: "European Seabass",
+        d: "Sword Fish",
+        ans: "ans4",
+    },
+
+    {
+        question: "What is the name of this flower",
+        a: "Billbergia",
+        b: "Echium",
+        c: "Blanket Flower",
+        d: "Lantana",
+        ans: "ans3",
+    },
+
+    {
+        question: "What is the name of this insect",
+        a: "Termite",
+        b: "Wasps",
+        c: "Ladybug",
+        d: "Booklice",
+        ans: "ans3",
+    },
+
+    {
+        question: "What is the name of this pet",
+        a: "Capybaras",
+        b: "Prairie dogs",
+        c: "Genets",
+        d: "Axolotls",
+        ans: "ans2",
+    },
+
+    {
+        question: "What is the name of this reptile",
+        a: "Spider",
+        b: "Bearded Dragon",
+        c: "Platypus",
+        d: "None of them",
+        ans: "ans2",
+    },
+
+    {
+        question: "What is the name of this sea-creature",
+        a: "Blackdragon Fish",
+        b: "Ghost Fish",
+        c: "Eerie Anglerfish",
+        d: "Giant Isopod",
+        ans: "ans1",
+    },
+
+    {
+        question: "What is the name of this tree",
+        a: "Eucalyptus",
+        b: "Banyan Tree",
+        c: "Aloe Vera Tree",
+        d: "Sal Tree",
+        ans: "ans2",
+    },
+
+    {
+        question: "What is the name of this wild animal",
+        a: "Hippopotamus",
+        b: "Mole",
+        c: "Reindeer",
+        d: "Panda",
+        ans: "ans4",
+    },
+]
